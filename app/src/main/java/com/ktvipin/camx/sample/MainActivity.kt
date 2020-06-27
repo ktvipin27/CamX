51616bac7c1be43aa06e9033c5c1cd421f23e164
VipinKT <ktvipin27@gmail.com>
2020-06-27 16:57:56 +0530

    sample updated

--- app/src/main/java/com/ktvipin/camx/sample/MainActivity.kt
+++ app/src/main/java/com/ktvipin/camx/sample/MainActivity.kt
@@ -15,14 +15,18 @@
  *
  */
 
-package com.ktvipin.camx
+package com.ktvipin.camx.sample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ktvipin.camx.CamX
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnOpenCamera.setOnClickListener { CamX.openCamera(this) }
     }
 }
